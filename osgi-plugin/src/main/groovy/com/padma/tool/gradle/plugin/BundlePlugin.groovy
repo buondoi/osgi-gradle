8d4338d1e76cb4dfef8382a6d59ec8d8042dbbee
tam nguyen <tamnguyen1560@gmail.com>
2016-05-10 22:07:43 +0700

    use String instead of GString.

--- osgi-plugin/src/main/groovy/com/padma/tool/gradle/plugin/BundlePlugin.groovy
+++ osgi-plugin/src/main/groovy/com/padma/tool/gradle/plugin/BundlePlugin.groovy
@@ -13,38 +13,39 @@ import java.nio.file.Path
 import static java.nio.file.Files.exists
 import static java.nio.file.Paths.get
 
+
 /**
- * Created on 5/9/16.
- */
+ * Created on 5/9/16.*/
 class BundlePlugin implements Plugin<ProjectInternal>
 {
-    @Override
-    void apply(final ProjectInternal project)
-    {
-        project.plugins.apply(JavaPlugin)
-        project.plugins.withType(JavaPlugin) {
-            def main = project.convention.getPlugin(JavaPluginConvention).sourceSets.main
+	@Override
+	void apply(final ProjectInternal project)
+	{
+		project.plugins.apply(JavaPlugin)
+		project.plugins.withType(JavaPlugin) {
+			def main = project.convention.getPlugin(JavaPluginConvention).sourceSets.main
 
-            Path root = get(main.output.classesDir.toURI())
-            def builder = new ManifestBuilder(root)
+			Path root = get(main.output.classesDir.toURI())
+			def builder = new ManifestBuilder(root)
 
-            // configure customized headers.
-            def props = ['Bundle-SymbolicName': "${project.name}", 'Bundle-Version': "${project.version.toString()}", 'Bundle-Name': "${project.name}"]
-            builder.withProps(props)
+			// configure customized headers.
+			def props = ["Bundle-SymbolicName": project.name, "Bundle-Version": project.version.
+					toString(), "Bundle-Name"   : project.name] as Map
+			builder.withProps(props)
 
-            // configure customized instructions.
-            main.resources.srcDirs.each { dir ->
-                def manifestPath = get(dir.toURI()).resolve('META-INF/manifest.yml')
-                if (exists(manifestPath))
-                {
-                    def instructions = Instructions.fromYml(manifestPath.toUri().toURL())
-                    builder.with(instructions)
-                }
-            }
+			// configure customized instructions.
+			main.resources.srcDirs.each {dir ->
+				def manifestPath = get(dir.toURI()).resolve('META-INF/manifest.yml')
+				if (exists(manifestPath))
+				{
+					def instructions = Instructions.fromYml(manifestPath.toUri().toURL())
+					builder.with(instructions)
+				}
+			}
 
-            project.tasks.withType(Jar) {
-                it.setManifest(new BundleManifest(project.fileResolver, builder))
-            }
-        }
-    }
+			project.tasks.withType(Jar) {
+				it.setManifest(new BundleManifest(project.fileResolver, builder))
+			}
+		}
+	}
 }
